1707f45cb3acd2fd6473d959025d43014c795580
Mineokah8 <mineokah8@gmail.com>
2025-04-14 09:14:23 +0100

    new message

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Container } from "@/components/Container";
 
 export function Footer() {
-  const navigation = ["Product", "Features", "Pricing", "Company", "Blog"];
+  const navigation = ["Home", "About", "Services", "Contact"];
   const legal = ["Terms", "Privacy", "Legal"];
   return (
     <div className="relative">
